3fbd2e2cce27e52714ddb018e32c74a6376f21b3
Hawi Odhiambo <hawiodhiambo@yahoo.com>
2020-07-15 15:35:19 +0300

    Initial commit. Used ReactCalendar to create a calendar Linked the calendar to a board that shows data Created a navigation bar

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './App.css';
+import {excel_data} from "./data/excel_data.js";
+import CovidInfo from "./CovidInfo"; 
+
+function App() {
+
+  const [date, setDate]= useState([new Date()]);
+  const [clickedDate, setClickedDate] = useState("");
+  const [dataIndex, setDataIndex] =useState("");
+
+  const dates_excel = excel_data.map((element, index) => {
+    if(index!==0){
+        return moment("1899/12/30", "YYYY/MM/DD").add(Number(element[0]), 'days').calendar()
+    }
+  });
+
+  const [tableRows, setTableRows]=useState(6);
+  const [buttonMessage, setButtonMessage] = useState("See More");
+  const [buttonStatus, setButtonStatus]=useState(true);
+
+  const defaultTableFormat = () => {
+         setTableRows(6)
+          setButtonMessage("See More")
+          setButtonStatus(true);
+  }
+
+  const modifyTableRows = () =>{
+    
+    if(buttonStatus){
+      const dataLength = excel_data[0].length;
+      setTableRows(dataLength)
+      setButtonMessage("See Less")
+      setButtonStatus(!buttonStatus);
+    }
+    else{
+      defaultTableFormat();
+    }
+    
+
+  }
+
+  const defaultDataFormat = (dataIndex, theClickedDate) =>{
+    setClickedDate(theClickedDate);
+    setDataIndex(dataIndex);
+    defaultTableFormat();
+  }
+
+ 
+
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg mdc-elevation--z7 mb-3">
+
+    </nav>
+    <div className="container" >
+      
+      <div className="row">
+        <div className="col-sm-12 mb-3"><DateCard clickedDate={clickedDate}/></div>
+        <div className="col-md-5">
+          <Calendar 
+            className="mdc-elevation--z2"
+            value={date}
+            tileClassName={({ date, view }) => {
+                           
+              if(view==="month"){
+       
+                dates_excel.splice(0,1, "undefined"); //removes this added data point.
+                if(dates_excel.find(dataDate => new Date(dataDate).toDateString()===date.toDateString())){
+                                             
+                    return  'highlight'
+                }
+              }
+              else{
+                dates_excel.splice(0,1,"03/01/2020"); //makes sure the month is highlighted in year view
+               
+                if(dates_excel.find(dataDate => new Date(dataDate).toDateString()===date.toDateString())){
+                                
+                    return  'highlight'
+                }
+               }
+                
+             }}
+             onClickDay ={(value, a) =>{
+              
+               if(a.currentTarget.className.search("highlight")!== -1){
+                
+                 dates_excel.find((date, index) =>{
+                    
+                   if(new Date(date).toDateString()===value.toDateString()){
+                    
+                    
+                    const theClickedDate = value.toDateString();
+                    const dataIndex = index;
+                    
+                    defaultDataFormat(dataIndex, theClickedDate)
+                   }
+                 })
+                 
+               }
+               else{
+                 setClickedDate("")
+               }
+              }} 
+          />
+        </div>
+        <div className="col-sm-7 mdc-elevation--z2 p-5">
+            <CovidInfo index={dataIndex} tableRows={tableRows} modifyTableRows={modifyTableRows} buttonMessage={buttonMessage}/>
+        </div>
+      </div>
+        
+    </div>
+    </div>
+  
+  );
+}
+function DateCard(props){
+  const style = {
+    //background: "#17ACBD",
+    //color: "white",
+    textAlign:"center"
+  }
+  return(
+    <div>
+      <h1 style={style}> {props.clickedDate}</h1>
+    </div>
+  )
+
+}
+
+export default App;
---
+++ src/CovidInfo.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import {excel_data} from "./data/excel_data.js";
+
+function CovidInfo(props){
+  
+    const [dataList, setDataList] = useState([]);
+   
+    const modifyTable = () =>{
+        
+        props.modifyTableRows()
+        
+    }
+    
+  
+    useEffect(()=>{
+    //populate the table
+        console.log("effect")
+        if(props.index!==""){
+            const data =[];
+            for(let i =1; i<props.tableRows; i++){
+            
+                data.push(<tr key={"tr"+i}>
+                                <th key={"th"+i}>{excel_data[0][i]}</th>
+                                <td key={"td"+i}>{excel_data[props.index][i]}</td>
+                            </tr>)
+                
+
+             }
+           setDataList(data) 
+        }
+        
+        
+    },[props.index, props.tableRows] )
+  
+ 
+    
+    return(
+      
+         (props.index !== "") ? (
+          <div>
+              <table className="table">
+              <tbody>
+                {dataList}
+              </tbody>
+            </table>
+         <button className="btn btn-info btn-block" onClick={modifyTable}>{props.buttonMessage}</button>
+          </div>
+          
+  
+         ): false
+  
+        
+  
+  
+      
+    )
+  }
+  export default CovidInfo;  
