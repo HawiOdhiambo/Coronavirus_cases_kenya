59c858fe583a8574e62af2a98498468643901aed
Hawi Odhiambo <hawiodhiambo@yahoo.com>
2020-07-25 21:46:25 +0300

    Fixed a navigation bar error - Gave the navigation anchor a valid link

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand-lg mdc-elevation--z7 mb-3">
         <div className="container">
-          <a className="navbar-brand" href="#">COVID-19 KENYA</a>
+          <a className="navbar-brand" href="/">COVID-19 KENYA</a>
         </div>
       </nav>
       <div className="container" >
